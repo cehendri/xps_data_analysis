b6f9376bd07c9c0e83be090251930604b1a5f5e3
Patrick Stanley <28008636+patrickstanley@users.noreply.github.com>
2017-10-04 16:04:08 -0400

    created process_data to bring together various individual functions

---
+++ process_data.py
@@ -0,0 +1,18 @@
+#Accumulation of all of our work. Just an example.
+
+#Import Data
+from xps_data_extractor import XPSImporter
+xps_importer = XPSImporter()
+result = xps_importer.load()
+
+#Normalize
+
+#Shift
+
+#Fit Individual
+
+#Fit Multiple
+
+#Identify Outliers
+
+return result
