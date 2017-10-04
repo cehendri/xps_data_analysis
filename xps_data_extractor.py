7d61fd2a6b6a21852c2bdde1b180edf20efa5a87
OlgaGKononova <OlgaGKononova@yandex.ru>
2017-10-04 16:37:42 -0400

    modified xps_data_extractor now can read xls and csv files with any amount of experimental data columns with and without shift

--- xps_data_extractor.py
+++ xps_data_extractor.py
@@ -7,7 +7,7 @@ import numpy as np
 
 class XPSImporter:
 
-    def __init__(self, dir_path='/home/olga/Documents/ECSHackDay/XPSprojext/xps_data_analysis/', xps_input_mask='Multiplex Data', xps_input_format='xlsx', elements=['O', 'F', 'C']):
+    def __init__(self, dir_path, xps_input_mask='Multiplex Data', xps_input_format='xlsx', elements=['C']):
         #Initialize and set up pathway to folder, filename mask and format of the data
 
         self.dir_path = dir_path
@@ -26,13 +26,14 @@ class XPSImporter:
         for element in self.elements:
             self.input_files[element] = []
 
-# TODO: less hardcoded reading of filenames and extracting elements from filenames
+        # TODO: less hardcoded reading of filenames and extracting elements from filenames
         for file in glob.glob("*." + self.xps_input_format):
             if self.xps_input_mask in file:
                 self.input_files[file[17]].append(file)
         print('Found files', self.input_files)
 
         output_structure = {}
+
         for element in self.elements:
             print('Extracting data for ', element)
             print('Found ', len(self.input_files[element]), 'files.')
@@ -40,12 +41,20 @@ class XPSImporter:
             if len(self.input_files[element]) > 0:
                 for file in self.input_files[element]:
                     print('\tReading ', file)
-                    data = pd.read_excel(self.dir_path + '/' + file)
+
+                    if 'xls' in self.xps_input_format:
+                        data = pd.read_excel(self.dir_path + '/' + file)
+                    if 'csv' in self.xps_input_format:
+                        data = pd.read_csv(self.dir_path + '/' + file)
+
+                    columns_count = len([name for name in data.keys() if 'unnamed' not in name.lower()])
                     columns = data.keys()
-#TODO: reading arbitrary amount of columns
-                    output_structure[element][columns[0]] = np.array(
-                        [[d for d in data[columns[1]]], [d for d in data[columns[2]]]])
-                    output_structure[element][columns[3]] = np.array(
-                        [[d for d in data[columns[4]]], [d for d in data[columns[5]]]])  # 5 and 6
+
+                    for column in range(columns_count):
+                        init_column_id = round(column * len(columns) / columns_count)
+                        column_id = round((column + 1) * len(columns) / columns_count) - 1
+                        output_structure[element][columns[init_column_id]] = np.array(
+                            [[d for d in data[columns[column_id - 1]]],
+                             [d for d in data[columns[column_id]]]])
 
         return output_structure
